9018a8cfe3cf65945d14733995900a7a7f1c838c
Anthony Ugwu <anthonyugwu234@gmail.com>
2018-11-30 17:42:56 +0100

    create profile page

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -9,9 +9,39 @@ import { signout }from '../actions/authAction';
 
       }
   render() {
-    return <div>profile
-        <button onClick={this.logout}>Logout</button>
-    </div>;
+   return(
+    <div class="bg body">
+    <header class="header2">
+
+        <nav>
+            <ul class="nav nav2">
+                <li>
+                    <a href="index.html">Home</a>
+                </li>
+                <li>
+                    <a href="#" id="logout">Sign Out</a>
+                </li>
+
+            </ul>
+
+        </nav>
+
+    </header>
+    <section class="profile">
+        <div class="bigbox">
+            <div class="box card span21 span2-center">
+                <h2 id="username">Hello Anthony</h2>
+                <h4>Total Number of Entries</h4>
+                <p id="totalEntry"></p>
+
+
+                <a href="add_entry.html" class="ebtn ebtn-filled">Add Entry</a>
+                <a href="entries.html" class="ebtn ebtn-transparent">My Diary</a>
+            </div>
+            </div>
+    </section>
+</div>
+   )
   }
 }
 export default connect(
